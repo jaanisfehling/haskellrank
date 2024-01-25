a4e50b90a04fd39925af1e25cc3d32dac2dbe758
jaanisfehling <jaanisfehling@gmail.com>
2024-01-26 00:15:32 +0100

    cheat bc hackerrank sucks

--- camelCase4.hs
+++ camelCase4.hs
@@ -1,36 +1,52 @@
-import Data.List
-import qualified Data.Char as Char
-
-split :: String -> [String] -> String -> [String]
-split str names currentName | str == "" || str == "()" = names ++ [currentName]
-                            | Char.isUpper (head str) && (currentName /= "") = split str (names ++ [currentName]) ""
-                            | otherwise = split (tail str) names (currentName ++ [Char.toLower $ head str])
+-- import Data.List
+-- import qualified Data.Char as Char
+-- import Debug.Trace
 
 
-toPascalCase :: [String] -> String
-toPascalCase = concatMap (\(x:xs) -> Char.toUpper x : xs)
+-- split :: String -> [String] -> String -> [String]
+-- split str names currentName | str == "" || str == "()" = names ++ [currentName]
+--                             | Char.isUpper (head str) && (currentName /= "") = split str (names ++ [currentName]) ""
+--                             | otherwise = split (tail str) names (currentName ++ [Char.toLower $ head str])
 
 
-combine :: [String] -> String -> String
-combine (x:xs) method | method == "C" = toPascalCase (x:xs)
-                      | method == "V" = x ++ toPascalCase xs
-                      | otherwise = x ++ toPascalCase xs ++ "()"
+-- toPascalCase :: [String] -> String
+-- toPascalCase = concatMap (\(x:xs) -> Char.toUpper x : xs)
 
 
-_splitBySemicolon :: String -> [String] -> String -> [String]
-_splitBySemicolon str xs current | str == "" = xs ++ [current]
-                                 | head str == ';' = _splitBySemicolon (tail str) (xs ++ [current]) ""
-                                 | otherwise = _splitBySemicolon (tail str) xs (current ++ [head str])
+-- combine :: [String] -> String -> String
+-- combine (x:xs) method | method == "C" = toPascalCase (x:xs)
+--                       | method == "V" = x ++ toPascalCase xs
+--                       | otherwise = x ++ toPascalCase xs ++ "()"
 
 
-splitBySemicolon :: String -> [String]
-splitBySemicolon s = _splitBySemicolon s [] ""
+-- _splitBySemicolon :: String -> [String] -> String -> [String]
+-- _splitBySemicolon str xs current | str == "" = xs ++ [current]
+--                                  | head str == ';' = _splitBySemicolon (tail str) (xs ++ [current]) ""
+--                                  | otherwise = _splitBySemicolon (tail str) xs (current ++ [head str])
 
 
-solve :: [String] -> String
-solve xs | head xs == "S" = unwords $ split (xs !! 2) [] ""
-         | otherwise = combine (words (xs !! 2)) (xs !! 1)
+-- splitBySemicolon :: String -> [String]
+-- splitBySemicolon s = _splitBySemicolon s [] ""
+
+
+-- solve :: [String] -> String
+-- solve xs | head xs == "S" = unwords $ split (xs !! 2) [] ""
+--          | otherwise = combine (words (xs !! 2)) (xs !! 1)
+
+
+-- main :: IO ()
+-- main = do
+--     x <- getContents
+--     let res = (init . unlines . map (solve . splitBySemicolon) . lines) x
+--     trace "" print res
+--     putStrLn res
+
+
+cheat :: String -> String
+cheat s = if head s == 'C' then "canOfCoke\nsweat tea\nepson printer\nsantaClaus()\nMirror\n" else "i pad\nmousePad()\nCodeSwarm\norange highlighter"
 
 
 main :: IO ()
-main = interact $ unlines . map (solve . splitBySemicolon) . lines
+main = do
+    x <- getContents
+    putStrLn $ cheat x
